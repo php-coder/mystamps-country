baa0ff2b168c514db4eee2f5c6a3238b7b6d5615
Slava Semushin <slava.semushin@gmail.com>
2019-11-25 21:04:20 +0000

    chore: output a message during startup.
    Also configure logging to include path to the source file. Example:
    
    2019/11/25 20:57:38 main.go:13: Running the server on port 8080

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
+import "log"
 import "net/http"
 
 func main() {
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("ok"))
 	})
+
+	log.Println("Running the server on port 8080")
+
 	http.ListenAndServe(":8080", nil)
 }
