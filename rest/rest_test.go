6597019d7f955b20e24ef0da625a4afbbcb1df8b
Slava Semushin <slava.semushin@gmail.com>
2020-01-04 15:49:58 +0000

    test: add a unit test for handling errors from database.
    Part of #2

--- rest/rest_test.go
+++ rest/rest_test.go
@@ -1,5 +1,6 @@
 package rest
 
+import "errors"
 import "testing"
 import "net/http"
 import "net/http/httptest"
@@ -19,4 +20,30 @@ func TestCountHandler_InvalidMethod(t *testing.T) {
 	if resp.StatusCode != http.StatusMethodNotAllowed {
 		t.Errorf("unexpected response status: expected %v, got %v", http.StatusMethodNotAllowed, resp.StatusCode)
 	}
+}
+
+func TestCountHandler_DbError(t *testing.T) {
+	// given
+	rec := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, "/ignored/mapping", nil)
+
+	db := &countryDBStub{errorMessage: "expected error from stub"}
+	// when
+	New(db).countHandler(rec, req)
+	// then
+	resp := rec.Result()
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusInternalServerError {
+		t.Errorf("unexpected response status: expected %v, got %v", http.StatusInternalServerError, resp.StatusCode)
+	}
+}
+
+type countryDBStub struct {
+	result       int
+	errorMessage string
+}
+
+func (c countryDBStub) CountAll() (int, error) {
+	return c.result, errors.New(c.errorMessage)
 }
